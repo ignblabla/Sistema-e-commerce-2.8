29468e1588e323151af21a3f8cbd1d1c39ee2c24
nataliaaolmo <natolmvil@alum.us.es>
2024-11-21 13:43:46 +0100

    feat: product details implemented

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -15,7 +15,6 @@ def store(request):
 		items = order.orderitem_set.all()
 		cartItems = order.get_cart_items
 	else:
-		#Create empty cart for now for non-logged in user
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
@@ -54,38 +53,11 @@ def checkout(request):
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/checkout.html', context)
 
-# def updateItem(request):
-# 	data = json.loads(request.body)
-# 	productId = data['productId']
-# 	action = data['action']
-# 	print('Action:', action)
-# 	print('Product:', productId)
-
-# 	customer = request.user
-# 	product = Product.objects.get(id=productId)
-# 	order, created = Order.objects.get_or_create(user=customer, complete=False)
-
-# 	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
-
-# 	if action == 'add':
-# 		orderItem.quantity = (orderItem.quantity + 1)
-# 	elif action == 'remove':
-# 		orderItem.quantity = (orderItem.quantity - 1)
-
-# 	orderItem.save()
-
-# 	if orderItem.quantity <= 0:
-# 		orderItem.delete()
-
-# 	return JsonResponse('Item was added', safe=False)
-
 def updateItem(request):
     data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
 
-    # Lógica para actualizar el carrito
-    # Asumiendo que tienes un modelo llamado Order o similar
     order, created = Order.objects.get_or_create(user=request.user, complete=False)
     product = Product.objects.get(id=productId)
     order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
@@ -170,3 +142,13 @@ def products_by_category(request, category_id):
         'selected_manufacturer': manufacturer_id,
     }
     return render(request, 'store/products_by_category.html', context)
+
+def product_details(request, product_id):
+	product= get_object_or_404(Product, id=product_id)
+    
+	context = {
+        'product': product,
+    }
+
+	return render(request,'store/product_details.html', context)
+
