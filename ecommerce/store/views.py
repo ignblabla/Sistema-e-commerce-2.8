d9953b4b299df42104b38c960856594bc730ff7e
Adrian <91976749+Adrcabmar@users.noreply.github.com>
2024-11-21 16:23:30 +0100

    fix: merge

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -16,6 +16,7 @@ def store(request):
 		items = order.orderitem_set.all()
 		cartItems = order.get_cart_items
 	else:
+		#Create empty cart for now for non-logged in user
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
@@ -54,11 +55,38 @@ def checkout(request):
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/checkout.html', context)
 
+# def updateItem(request):
+# 	data = json.loads(request.body)
+# 	productId = data['productId']
+# 	action = data['action']
+# 	print('Action:', action)
+# 	print('Product:', productId)
+
+# 	customer = request.user
+# 	product = Product.objects.get(id=productId)
+# 	order, created = Order.objects.get_or_create(user=customer, complete=False)
+
+# 	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+# 	if action == 'add':
+# 		orderItem.quantity = (orderItem.quantity + 1)
+# 	elif action == 'remove':
+# 		orderItem.quantity = (orderItem.quantity - 1)
+
+# 	orderItem.save()
+
+# 	if orderItem.quantity <= 0:
+# 		orderItem.delete()
+
+# 	return JsonResponse('Item was added', safe=False)
+
 def updateItem(request):
     data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
 
+    # Lógica para actualizar el carrito
+    # Asumiendo que tienes un modelo llamado Order o similar
     order, created = Order.objects.get_or_create(user=request.user, complete=False)
     product = Product.objects.get(id=productId)
     order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
@@ -91,7 +119,7 @@ def processOrder(request):
 
 		if order.shipping == True:
 			ShippingAddress.objects.create(
-			user=customer,
+			customer=customer,
 			order=order,
 			address=data['shipping']['address'],
 			city=data['shipping']['city'],
@@ -143,6 +171,7 @@ def products_by_category(request, category_id):
         'selected_manufacturer': manufacturer_id,
     }
     return render(request, 'store/products_by_category.html', context)
+
 def product_details(request, product_id):
 	product= get_object_or_404(Product, id=product_id)
     
@@ -151,7 +180,6 @@ def product_details(request, product_id):
     }
 
 	return render(request,'store/product_details.html', context)
-
 @login_required
 def list_orders(request):
     user = request.user
